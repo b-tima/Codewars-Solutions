0f17a3cb9c247d229d8c697f45b68fa5f3941994
Morgan Adamsson <60752260+morganadKTH@users.noreply.github.com>
2021-03-28 23:17:21 +0200

    better one liner

--- Python/Stop-gninnipS-My-sdroW!.py
+++ Python/Stop-gninnipS-My-sdroW!.py
@@ -1,2 +1,2 @@
 def spin_words(sentence):
-    return "".join((word if len(word) < 5 else word[::-1]) + " " for word in sentence.split())[0:-1]
+    return " ".join((word if len(word) < 5 else word[::-1]) for word in sentence.split())
