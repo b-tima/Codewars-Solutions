519523bb78d7cc05cc0c63bba544d42039d1e3f9
b-tima <morgan.a9904@gmail.com>
2021-08-27 23:13:53 +0200

    Refactoring

--- Python/Esolang Interpreters #4 Boolfuck Interpreter.py
+++ Python/Esolang Interpreters #4 Boolfuck Interpreter.py
@@ -50,107 +50,113 @@ class Interpreter:
         self.cached_loop = {}
         self.cached_pointer = {}
 
-    def getOutput(self):
+    def get_output(self):
         return Interpreter.convert_to_string(self.output)
 
-    def bitFlip(self):
+    def bit_flip(self):
         bit = self.memory[self.memory_pointer]
-        self.memory[self.memory_pointer] = True if not bit else False
-        self.instruction_pointer += 1
+        self.memory[self.memory_pointer] = not bit
 
-    def readInput(self):
+    def read_input(self):
         # Reads a 0 if input stream is exhausted
         if self.input_pointer >= len(self.input):
-            self.instruction_pointer += 1
             self.memory[self.memory_pointer] = False
             return
 
         self.memory[self.memory_pointer] = self.input[self.input_pointer]
         self.input_pointer += 1
-        self.instruction_pointer += 1
 
-    def outputBit(self):
+    def output_bit(self):
         self.output.append(self.memory[self.memory_pointer])
-        self.instruction_pointer += 1
 
-    def moveMemory(self, direction, instruction):
-        # Checks cache if number of rows have already been enumerated
-        if self.instruction_pointer in self.cached_pointer:
-            self.memory_pointer += (
-                direction * self.cached_pointer[self.instruction_pointer]
-            )
-
-            # Initalize memory
-            if self.memory_pointer not in self.memory:
-                self.memory[self.memory_pointer] = False
-
-            self.instruction_pointer += self.cached_pointer[self.instruction_pointer]
-            return
-
-        count = 0
-        first_pointer = self.instruction_pointer
-
-        # Counts the number of memory increments in a row
-        while (
-            self.instruction_pointer < len(self.code)
-            and self.code[self.instruction_pointer] == instruction
-        ):
-            self.instruction_pointer += 1
-            count += 1
-        self.memory_pointer += direction * count
-
-        # Initialize memory
+    def initiate_memory_address(self, address):
         if self.memory_pointer not in self.memory:
             self.memory[self.memory_pointer] = False
 
-        self.cached_pointer[first_pointer] = count
+    def move_memory_and_instruction_pointer_by_cached_steps(self, direction):
+        steps = self.cached_pointer[self.instruction_pointer]
+        self.memory_pointer += direction * steps
+        self.instruction_pointer += steps - 1
+        self.initiate_memory_address(self.memory_pointer)
 
-    def incrementMemory(self):
-        self.moveMemory(1, '>')
+    def get_reoccouring_step_count(self, start_pointer, step):
+        # Counts the number of memory increments in a row
+        pointer = start_pointer
+        while pointer < len(self.code) and self.code[pointer] == step:
+            pointer += 1
+        return pointer - start_pointer
 
-    def decrementMemory(self):
-        self.moveMemory(-1, '<')
+    def move_memory_and_instruction_pointer_by_memory_instruction(
+        self, direction, instruction
+    ):
+        # Checks cache if memory string has already been enumerated
+        if self.instruction_pointer in self.cached_pointer:
+            self.move_memory_and_instruction_pointer_by_cached_steps(direction)
+            return
 
-    def moveLoop(self, b1, b2, direction, condition):
-        if condition:
-    
-            # Check cache for loopend
-            if self.instruction_pointer in self.cached_loop:
-                self.instruction_pointer = self.cached_loop[self.instruction_pointer] + 1
-                return
-            
-            # Count number of brackets
-            count = 1
-            first_pointer = self.instruction_pointer
-            while count > 0:
-                self.instruction_pointer += direction
-                if self.code[self.instruction_pointer] == b1:
-                    count += 1
-                elif self.code[self.instruction_pointer] == b2:
-                    count -= 1
+        step_count = self.get_reoccouring_step_count(
+            self.instruction_pointer, instruction
+        )
+        self.memory_pointer += direction * step_count
+        self.cached_pointer[self.instruction_pointer] = step_count
+        self.instruction_pointer += step_count - 1
 
-            # Cache both loopstart and loopend
-            self.cached_loop[first_pointer] = self.instruction_pointer
-            self.cached_loop[self.instruction_pointer] = first_pointer
+        # Initialize memory
+        self.initiate_memory_address(self.memory)
 
-        self.instruction_pointer += 1
+    def increment_memory(self):
+        self.move_memory_and_instruction_pointer_by_memory_instruction(1, ">")
 
+    def decrement_memory(self):
+        self.move_memory_and_instruction_pointer_by_memory_instruction(-1, "<")
 
-    def loopStart(self):
-        self.moveLoop('[', ']', 1, self.memory[self.memory_pointer] == False)
+    def find_loop_edges(self, start_pointer, loop_start_edge, loop_end_edge, direction):
+        # Count number of edges
+        count = 1
+        first_pointer = start_pointer
+        pointer = start_pointer
+        while count > 0:
+            pointer += direction
+            if self.code[pointer] == loop_start_edge:
+                count += 1
+            elif self.code[pointer] == loop_end_edge:
+                count -= 1
+        return first_pointer, pointer
 
-    def loopEnd(self):
-        self.moveLoop(']', '[', -1, self.memory[self.memory_pointer] == True)
+    def move_instruction_pointer_to_loop_edge(
+        self, loop_start_edge, loop_target_edge, direction
+    ):
+        # Check if current instruction pointer is cached
+        if self.instruction_pointer in self.cached_loop:
+            self.instruction_pointer = self.cached_loop[self.instruction_pointer]
+            return
 
-    def nextInstruction(self):
+        loop_start, loop_end = self.find_loop_edges(
+            self.instruction_pointer, loop_start_edge, loop_target_edge, direction
+        )
+        self.instruction_pointer = loop_end
+
+        # Cache both loop_start and loop_end
+        self.cached_loop[loop_start] = loop_end
+        self.cached_loop[loop_end] = loop_start
+
+    def loop_start(self):
+        if self.memory[self.memory_pointer] == False:
+            self.move_instruction_pointer_to_loop_edge("[", "]", 1)
+
+    def loop_end(self):
+        if self.memory[self.memory_pointer] == True:
+            self.move_instruction_pointer_to_loop_edge("]", "[", -1)
+
+    def next_instruction(self):
         instruction_set = {
-            "+": self.bitFlip,
-            ",": self.readInput,
-            ";": self.outputBit,
-            "<": self.decrementMemory,
-            ">": self.incrementMemory,
-            "[": self.loopStart,
-            "]": self.loopEnd,
+            "+": self.bit_flip,
+            ",": self.read_input,
+            ";": self.output_bit,
+            "<": self.decrement_memory,
+            ">": self.increment_memory,
+            "[": self.loop_start,
+            "]": self.loop_end,
         }
 
         if self.instruction_pointer >= len(self.code):
@@ -158,15 +164,14 @@ class Interpreter:
         instruction = self.code[self.instruction_pointer]
 
         # Ignore instruction not in instruction set
-        if instruction not in instruction_set:
-            self.instruction_pointer += 1
-        else:
+        if instruction in instruction_set:
             instruction_set[instruction]()
+        self.instruction_pointer += 1
         return True
 
 
 def boolfuck(code, input=""):
     interpreter = Interpreter(code, input)
-    while interpreter.nextInstruction():
+    while interpreter.next_instruction():
         pass
-    return interpreter.getOutput()
+    return interpreter.get_output()
